9c8aee1c6afe8ffc1bea92233c0fe41487a602b9
SwamynathanProfessional <swamynathan.bommaganty@gmail.com>
2021-11-08 15:53:16 +0530

    bb

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -4,22 +4,22 @@ export const CATEGORIES = [
     new Category(
         'Primary Product',
         'Microsoft Surface Go 2',
-        'Price$1,100*',
         'SKU# kI475...',
+        'Price$1,100*',
       ),
     
       new Category(
         'Primary Product',
         'Microsoft Surface Go 3',
-        'Price$1,100*',
         'SKU# kI475...',
-      ),
+        'Price$1,100*',
+        ),
     
       new Category(
         'Primary Product',
         'Microsoft Surface Go 4',
-        'Price$1,100*',
         'SKU# kI475...',
+        'Price$1,100*',
       ),
     
 
